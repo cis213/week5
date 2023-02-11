63e9c1baa5449a0dd4b5ef575f1f8c1400093fb0
frank stepanski <frankstepanski@franks-mac-mini.myfiosgateway.com>
2023-02-10 21:52:23 -0500

    Updated components

--- src/components/Calculation.jsx
+++ src/components/Calculation.jsx
@@ -10,7 +10,20 @@ const Calculation = ({ // passing three functions as props
     onSecondNumberChanged,
     onOperationChanged,
   }) => {
-    
+    /*
+      Controlled components:
+        - With state and form fields, you can perform two-way binding.
+        - This means that the state is updated when the form field is changed,
+        -   and the form field is updated when the state is changed.
+        - Or in other words, the state is the source of truth.
+        - This is also known as controlled components.
+
+      Uncontrolled components:
+       - Uncontrolled components are the opposite of controlled components.
+       - The state is not updated when the form field is changed.
+       - The example below is an uncontrolled component.
+
+    */
     return (
       <p>
         {/*  updates state for first number entered */} 
